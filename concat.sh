c12960dddf5e5915ab5ac05bae4d9b58a057cf49
Antcating <yevhenii.fedorchenko@gmail.com>
2023-12-05 10:09:56 +0200

    feat: :sparkles: bash wrapper for project

---
+++ concat.sh
@@ -0,0 +1,6 @@
+# Changing directory to main project directory
+pushd PROJECT_PATH
+# Activate venv FOR BASH (if you are using FISH, then use activate.fish)
+source .venv_concat/bin/activate
+# Running project
+python3 main.py
