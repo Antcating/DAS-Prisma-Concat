718569d6b874fcf8c3f3a18a2235af0fe626ea18
Antcating <yevhenii.fedorchenko@gmail.com>
2023-12-31 17:43:35 +0200

    feat: :ambulance: Logging on critical errors

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 from Concatenator import PrismaConcatenator
-
+from log.main_logger import logger as log
 from config import INPUT_PATH, OUTPUT_PATH
 
 concatenator = PrismaConcatenator(INPUT_PATH, OUTPUT_PATH)
-concatenator.run()
+try:
+    concatenator.run()
+except Exception as e:
+    log.error(e)
+    # Print traceback to console
+    import traceback
+    traceback.print_exc()
